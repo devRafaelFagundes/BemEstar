ba3300b988e6736ffd870ed294526fc4759df217
devRafaelFagundes <devrafaelfagundes@gmail.com>
2025-05-31 14:20:50 -0300

    Database connected
    properly connection with mongoDb atlas  and implementation on the server

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const connectToDb = require("./database/db");
 const path = require("path");
 const homeRouter = require("./routes/beforeLogin");
 const app = express();
 
 app.set('view engine', 'ejs');
-
 app.use(express.urlencoded());
-app.use(express.static(path.join(__dirname, "views")));
 
+//views and public folders
+app.set("views", path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routers
 app.use('', homeRouter);
 
 app.listen(3000, ()=>{
     console.log("port 3000 in open state");
 })
+connectToDb();
