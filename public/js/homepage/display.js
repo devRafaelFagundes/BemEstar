445ac651338be8302d92978b87021b49fbe21002
devRafaelFagundes <devrafaelfagundes@gmail.com>
2025-06-25 00:22:10 -0300

    Display logic of homepage

--- public/js/homepage/display.js
+++ public/js/homepage/display.js
@@ -1,6 +1,23 @@
 const button = document.getElementById("menu");
 const sideBar = document.getElementById("sidebar")
-
 button.addEventListener("click", (e) => {
     sideBar.classList.toggle("show")
+})
+
+const selectors = document.querySelectorAll(".selector")
+const contents = document.querySelectorAll(".content")
+selectors.forEach((selected) => {
+    selected.addEventListener("click", e => {
+        let indice = 0
+        for (let i = 0; i < selectors.length; i++) {
+            if(selectors[i] === selected) {
+                indice = i
+            }
+        }
+
+        contents.forEach((element) => {
+            element.classList.remove("display")
+        })
+        contents[indice].classList.add("display")
+    })
 })
